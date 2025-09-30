656c53be5714061d1dd7e18c2c80d3670150ee32
hamzah <hamzahnafalah@gmail.com>
2025-09-30 11:53:05 +0700

    update module patient

--- src/Database/Seeders/data/roles/front-office.php
+++ src/Database/Seeders/data/roles/front-office.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'api.patient-management.patient.*',
-    // 'api.patient-management.reservation.*',
-    // 'api.patient-management.letter-queue.*',
-    // 'api.patient-management.referral.*',
-    // 'api.patient-management.visit-patient.*',
-    // 'api.patient-management.admin-waiting-list.*',
+    'api.patient-emr.patient.*',
+    // 'api.patient-emr.reservation.*',
+    // 'api.patient-emr.letter-queue.*',
+    // 'api.patient-emr.referral.*',
+    // 'api.patient-emr.visit-patient.*',
+    // 'api.patient-emr.admin-waiting-list.*',
     // 'transaction.*'
 ];
